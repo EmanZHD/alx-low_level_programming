0346c65a79d65f2e57a9d15beb7768a81c449301
EmanZHD <zahidimane1@gmail.com>
2023-04-10 18:21:26 -0700

    task 1 done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the number of its arguments
+ * @argc: number of args
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+
+		i = 0;
+		while (*argv != NULL)
+		{
+
+			i++;
+			argv++;
+
+		}
+		printf("%d\n", i - 1);
+	}
+	return (0);
+}

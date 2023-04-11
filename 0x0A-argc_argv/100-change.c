aca92c56d8cccf6c49753c42ecf494bd0b0921cf
EmanZHD <zahidimane1@gmail.com>
2023-04-10 19:11:00 -0700

    task 5 done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,58 +1,72 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - prints the minimum number of coins to make change
- * for an amount of money
- * @argc: number of arguments
- * @argv: arguments
- * Return: returns 1 if there is an error; else returns 0
+ * _atoi - check the code
+ * @s: char *
+ * Return: int
  */
-
-int main(int argc, char **argv)
+int _atoi(char *s)
 {
-	int cents;
-	int coins;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+		if (isi == 1)
+		{
+			break;
+		}
+		c++;
+	}
+	ni *= min;
+	return (ni);
+}
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: char *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int coins[] = {25, 10, 5, 2, 1};
+	int i;
+	int result;
 
-	coins = 0;
 	if (argc != 2)
 	{
-
-
 		printf("Error\n");
-		return (1);
+		return (0);
 	}
-	cents = atoi(argv[1]);
-	while (cents > 0)
+	a = _atoi(argv[1]);
+	result = 0;
+	if (a < 0)
 	{
-
-		coins++;
-		if ((cents - 25) >= 0)
-		{
-
-			cents -= 25;
-			continue;
-		}
-		if ((cents - 10) >= 0)
-                {       
-
-                        cents -= 10;
-                        continue;
-                }
-		if ((cents - 5) >= 0)
-                {
-
-                        cents -= 5;
-                        continue;
-                }
-		if ((cents - 2) >= 0)
-                {       
-
-                        cents -= 2;
-                        continue;
-                }
-		cents--;
+		printf("0\n");
+		return (0);
 	}
-	printf("%d\n", coins);
+	for (i = 0; i < 5 && a >= 0; i++)
+	{
+		while (a >= coins[i])
+		{
+			result++;
+			a -= coins[i];
+		}
+	}
+	printf("%d\n", result);
 	return (0);
 }

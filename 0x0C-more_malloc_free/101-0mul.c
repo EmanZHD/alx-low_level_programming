b94d02e822c242257d9f1a29d308c33345eb570b
EmanZHD <zahidimane1@gmail.com>
2023-04-13 15:37:51 +0000

    task done

--- 0x0C-more_malloc_free/101-0mul.c
+++ 0x0C-more_malloc_free/101-0mul.c
@@ -5,11 +5,12 @@
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
- *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
+
 int is_digit(char *s)
 {
+
 	int i = 0;
 
 	while (s[i])
@@ -19,16 +20,18 @@ int is_digit(char *s)
 		i++;
 	}
 	return (1);
+
 }
 
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- *
- * Return: the length of the string
+ * Return: length of the string
  */
+
 int _strlen(char *s)
 {
+
 	int i = 0;
 
 	while (s[i] != '\0')
@@ -36,26 +39,31 @@ int _strlen(char *s)
 		i++;
 	}
 	return (i);
+
 }
 
 /**
  * errors - handles errors for main
  */
+
 void errors(void)
 {
+
 	printf("Error\n");
 	exit(98);
+
 }
 
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- *
- * Return: always 0 (Success)
+ * Return: always 0 (success)
  */
+
 int main(int argc, char *argv[])
 {
+
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
@@ -76,6 +84,7 @@ int main(int argc, char *argv[])
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
+
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
@@ -92,7 +101,7 @@ int main(int argc, char *argv[])
 			_putchar(result[i] + '0');
 	}
 	if (!a)
-		_putchar('0');
+	_putchar('0');
 	_putchar('\n');
 	free(result);
 	return (0);

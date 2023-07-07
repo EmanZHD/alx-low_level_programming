c7251827c44da91b30ea884ac5e4a643dc08f503
EmaneZHD <zahidimane1@gmail.com>
2023-07-07 18:35:21 +0000

    done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /*
- * hash_table_create - function that creates a hash table
+ * hash_table_create - creates a hash table
  * @size: the size of the array
  * Return: return pointer to the newly created hash table
  */
